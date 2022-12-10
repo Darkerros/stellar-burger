0ed869e5bc5f0f639c2b54e1c84fdf5656fb83da
Darkerros <darkerros@yandex.ru>
2022-12-11 02:27:07 +0300

    fix: move Provider to main file

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import appStyles from './App.module.css';
 import AppHeader from "../AppHeader/AppHeader";
 import AppMain from "../AppMain/AppMain";
-import {Provider} from "react-redux";
-import store from "../../services/store/store";
 
 
 function App() {
     return (
-        <Provider store={store}>
-            <div className={appStyles.App}>
+    <div className={appStyles.App}>
                 <AppHeader/>
                 <AppMain/>
             </div>
-        </Provider>
     );
 }
 
