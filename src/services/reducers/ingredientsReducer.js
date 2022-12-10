a552ac4745cfe5efb5950c6a0bcbd9e6578b7113
Darkerros <darkerros@yandex.ru>
2022-12-11 02:19:09 +0300

    fix: fix ingredient isSuccess state and naming

--- src/services/reducers/ingredientsReducer.js
+++ src/services/reducers/ingredientsReducer.js
@@ -2,16 +2,16 @@ const INGREDIENTS_LOADING = "INGREDIENTS_LOADING"
 const INGREDIENTS_GET = "INGREDIENTS_GET"
 const INGREDIENTS_GET_FAILED = "INGREDIENTS_GET_FAILED"
 
-const defaultState = {isLoading: false, isFail: false, isSucess: false, ingredients: []}
+const defaultState = {isLoading: false, isFail: false, isSuccess: false, ingredients: []}
 
 const ingredientsReducer = (state = defaultState, action) => {
     switch (action.type) {
         case INGREDIENTS_LOADING:
-            return {isLoading: true, isFail: false, isSucess: false, ...state}
+            return {isLoading: true, isFail: false, isSuccess: false, ...state}
         case INGREDIENTS_GET:
-            return {isLoading: false, isFail: false, isSucess: false, ingredients: action.payload}
+            return {isLoading: false, isFail: false, isSuccess: true, ingredients: action.payload}
         case INGREDIENTS_GET_FAILED:
-            return {isLoading: false, isFail: true, isSucess: false, ...state}
+            return {isLoading: false, isFail: true, isSuccess: false, ...state}
         default:
             return state
     }
@@ -21,4 +21,4 @@ export default ingredientsReducer
 
 export const ingredientsLoadAction = () => ({type: INGREDIENTS_LOADING})
 export const ingredientsLoadFailAction = () => ({type: INGREDIENTS_GET_FAILED})
-export const ingredientsGetAction = (ingredients) => ({type: ingredientsGetAction, payload: ingredients})
+export const ingredientsLoadCompleteAction = (ingredients) => ({type: INGREDIENTS_GET, payload: ingredients})
