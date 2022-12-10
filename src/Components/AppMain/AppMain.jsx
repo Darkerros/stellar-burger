bf5d517250c645f77350101e6330ba6c5e77c94f
Darkerros <darkerros@yandex.ru>
2022-12-10 19:53:47 +0300

    feat: add order and setOrder to context

--- src/Components/AppMain/AppMain.jsx
+++ src/Components/AppMain/AppMain.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useMemo, useReducer, useState} from 'react';
 import appMainStyles from './AppMain.module.css'
 import BurgerIngredients from "../BurgerIngredients/BurgerIngredients";
-import api from "../../api/Api";
+import api from "../../api/api";
 import BurgerConstructor from "../BurgerConstructor/BurgerConstructor";
 import Loading from "../Loading/Loading";
 import {getRandomItemIndex} from "../../utils/utils";
@@ -10,22 +10,23 @@ import CartContext from "../../context/CartContext";
 import IngredientsContext from "../../context/IngredientsContext";
 
 const AppMain = () => {
-        const [allIngredients, setAllIngredients] = useState([])
-        const [cart, cartDispatch] = useReducer(cartReducer, {bun: undefined, items: []})
+    const [allIngredients, setAllIngredients] = useState([])
+    const [cart, cartDispatch] = useReducer(cartReducer, {bun: undefined, items: []})
+    const [orderId,setOrderId] = useState(undefined)
 
-        useEffect(() => {
+    useEffect(() => {
             api.getrIngredients()
                 .then(ingredientsList => setAllIngredients(ingredientsList.data))
                 .catch(error => alert(error.message))
         }, [])
-        useEffect(() => {
+    useEffect(() => {
             const ingredientList = allIngredients.filter(ingredient => ingredient.type !== 'bun')
             const cartItems = ingredientList.slice(0, getRandomItemIndex(ingredientList))
             const cartWithKey = cartItems.map((item, index) => ({cartId: Date.now() + 100 * index, ...item})) || []
             cartDispatch(setCartAction(cartWithKey))
         }, [allIngredients])
 
-        const groupedIngredients = useMemo(() => {
+    const groupedIngredients = useMemo(() => {
             const groupIngredients = [
                 {type: 'bun', name: 'Булки', ingredientsList: []},
                 {type: "sauce", name: 'Соусы', ingredientsList: []},
@@ -43,28 +44,29 @@ const AppMain = () => {
             }
             return groupIngredients
         }, [allIngredients])
-        const ingredientsCounts = useMemo(() => {
+    const ingredientsCounts = useMemo(() => {
             const ingredientsCount = {}
             allIngredients.forEach((ingredient) => ingredientsCount[ingredient._id] = cart.items.filter(cartItem => cartItem._id === ingredient._id).length)
             // eslint-disable-next-line no-unused-expressions
             cart.bun ? ingredientsCount[cart.bun._id] = 1 : false
             return ingredientsCount
         }, [allIngredients, cart])
-        const getIngredientCount = (ingredientId) => ingredientsCounts[ingredientId]
+    const getIngredientCount = (ingredientId) => ingredientsCounts[ingredientId]
 
-        return (
-            allIngredients.length === 0
-                ?
-                <Loading/>
-                :
-                <main className={appMainStyles.AppMain}>
-                    <IngredientsContext.Provider value={{getIngredientCount,groupedIngredients}}>
-                        <BurgerIngredients />
-                    </IngredientsContext.Provider>
-                    <CartContext.Provider value={{cart,cartDispatch}}>
-                        <BurgerConstructor />
-                    </CartContext.Provider>
-                </main>
+
+return (
+    allIngredients.length === 0
+    ?
+        <Loading/>
+    :
+        <main className={appMainStyles.AppMain} >
+            <IngredientsContext.Provider value={{getIngredientCount, groupedIngredients}}>
+                <BurgerIngredients/>
+            </IngredientsContext.Provider>
+            <CartContext.Provider value={{cart: cart, cartDispatch:cartDispatch,orderId:orderId,setOrderId:setOrderId}}>
+                <BurgerConstructor/>
+            </CartContext.Provider>
+        </main>
         );
     }
 ;
--- src/Components/CartInfo/CartInfo.jsx
+++ src/Components/CartInfo/CartInfo.jsx
@@ -1,20 +1,28 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Button} from "@ya.praktikum/react-developer-burger-ui-components";
 import moneyIcon from '../../images/icons/money-icon.png'
 import cartInfoStyles from './CartInfo.module.css'
 import PropTypes from "prop-types";
 import Modal from "../Modal/Modal";
 import OrderDetails from "../OrderDetails/OrderDetails";
+import CartContext from "../../context/CartContext";
+import api from "../../api/api";
 
 const CartInfo = ({cartPrice}) => {
     const [orderModalState, setOrderModalState] = useState(false)
-
+    const {orderId,setOrderId} = useContext(CartContext)
+    const {cart} = useContext(CartContext)
     const handleCloseOrderModal = () => {
         setOrderModalState(false)
     }
 
     const handleOpenOrderModal = () => {
         setOrderModalState(true)
+    }
+
+    const createOrder = () => {
+        const cartItemId = [cart.bun._id,...cart.items.map(item => item._id)]
+        api.createOrder(cartItemId).then(data => setOrderId(data.order.number)).then(() => handleOpenOrderModal())
     }
 
 
@@ -28,13 +36,13 @@ const CartInfo = ({cartPrice}) => {
                 type="primary"
                 size="large"
                 htmlType={'button'}
-                onClick={handleOpenOrderModal}
+                onClick={createOrder}
             >
                 Оформить заказ
             </Button>
             {orderModalState &&
                 <Modal handleClose={handleCloseOrderModal}>
-                    <OrderDetails orderId="034536"/>
+                    <OrderDetails orderId={orderId}/>
                 </Modal>
             }
         </div>
