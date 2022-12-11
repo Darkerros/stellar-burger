9e267b7f46fbc96652db565d950083c3cbaecbd5
Darkerros <darkerros@yandex.ru>
2022-12-11 17:55:28 +0300

    fix: add chekresponse func and universal create request func

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,21 @@
-const GET_INGREDIENTS_ENDPOINT = 'https://norma.nomoreparties.space/api/ingredients'
-const CREATE_ORDER_ENDPOINT = 'https://norma.nomoreparties.space/api/orders'
+import {checkResponse} from "../utils/utils";
+
+const BASE_URL = 'https://norma.nomoreparties.space'
+
+const createRequest = (endpoint, method, body = null) => {
+    const settings = {
+        method: method,
+        headers: {"Content-type": 'application/json'},
+    }
+    // eslint-disable-next-line no-unused-expressions
+    body ? settings.body = JSON.stringify(body) : false
+    return fetch(endpoint,settings)
+}
+
 
 const api = {
-    getrIngredients : () => fetch(GET_INGREDIENTS_ENDPOINT).then(res => res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`)),
-    createOrder : (ingredientsList) => fetch(CREATE_ORDER_ENDPOINT,
-        {method: "POST",
-                headers: {"Content-type": 'application/json'},
-                body: JSON.stringify({ingredients: ingredientsList})}).then(res => res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`))
-    }
+    getrIngredients: () => createRequest(`${BASE_URL}/api/ingredients`,"GET").then(checkResponse),
+    createOrder: (ingredientsList) => createRequest(`${BASE_URL}/api/orders`,"POST",{ingredients: ingredientsList}).then(checkResponse)
+}
 
 export default api;
