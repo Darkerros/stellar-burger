7e813f583c993f2e9f4d1ba73994f5e5253a288f
Darkerros <darkerros@yandex.ru>
2022-12-11 17:57:49 +0300

    fix: delete universal function

--- src/api/api.js
+++ src/api/api.js
@@ -1,17 +1,6 @@
-import {checkResponse} from "../utils/utils";
+import {checkResponse, createRequest} from "../utils/utils";
 
 const BASE_URL = 'https://norma.nomoreparties.space'
-
-const createRequest = (endpoint, method, body = null) => {
-    const settings = {
-        method: method,
-        headers: {"Content-type": 'application/json'},
-    }
-    // eslint-disable-next-line no-unused-expressions
-    body ? settings.body = JSON.stringify(body) : false
-    return fetch(endpoint,settings)
-}
-
 
 const api = {
     getrIngredients: () => createRequest(`${BASE_URL}/api/ingredients`,"GET").then(checkResponse),
