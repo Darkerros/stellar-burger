8dc0b679766979669d88ca1f95198b80f69efc3d
Darker <darkerros@yandex.ru>
2023-01-21 18:37:41 +0300

    feat: add main page

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,13 @@
+import AppHeader from "../../components/AppHeader/AppHeader";
+import AppMain from "../../components/AppMain/AppMain";
+
+const MainPage = () => {
+    return (
+        <>
+            <AppHeader/>
+            <AppMain/>
+        </>
+    );
+};
+
+export default MainPage;
