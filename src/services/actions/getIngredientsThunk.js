ac51bd528dbc142cb5528d1c622da582e085f877
Darkerros <Darkerros@yandex.ru>
2023-01-22 20:12:23 +0300

    Updates

--- src/services/actions/getIngredientsThunk.js
+++
@@ -1,9 +0,0 @@
-import api from "../../api/api";
-import {ingredientsLoadAction, ingredientsLoadCompleteAction, ingredientsLoadFailAction} from "./ingredientsActions";
-
-export const getIngredientsThunk = () => dispatch => {
-    dispatch(ingredientsLoadAction())
-    api.getrIngredients()
-        .then(data => dispatch(ingredientsLoadCompleteAction(data.data)))
-        .catch(() => dispatch(ingredientsLoadFailAction()))
-}
--- src/types/tabInfoType.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from "prop-types";
-
-const tabInfoType = PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
-})
-
-export default tabInfoType;
