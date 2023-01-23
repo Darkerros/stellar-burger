ac2cd16b74b45828a54599e7d4accff7666a623f
Darkerros <Darkerros@yandex.ru>
2023-01-23 22:27:40 +0300

    feat: add routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,38 @@
 import appStyles from './App.module.css';
 import ResetPasswordPage from "../../pages/ResetPasswordPage/ResetPasswordPage";
+import MainPage from "../../pages/MainPage/MainPage";
+import {useEffect} from "react";
+import Api from "../../api/Api";
+import useTokenStorage from "../../hooks/useTokenStorage";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RegisterPage from "../../pages/RegisterPage/RegisterPage";
+import ForgotPasswordPage from "../../pages/ForgotPasswordPage/ForgotPasswordPage";
+import LoginPage from "../../pages/LoginPage/LoginPage";
 
 
 function App() {
+    const {getToken} = useTokenStorage()
+
+
+    useEffect(() => {
+        Api.getUser(getToken())
+            .then(date => console.log(date))
+            .catch(err => console.log("Авторизация не удалась"))
+
+    },[])
+
+    const router = createBrowserRouter([
+        {path: '/', element: <MainPage/>},
+        {path: '/login', element: <LoginPage/>},
+        {path: "/register", element: <RegisterPage/>},
+        {path: "/reset", element: <ResetPasswordPage/>},
+        {path: "/forgot", element: <ForgotPasswordPage/>},
+        {path: "*", element: <MainPage/>}
+    ])
+
     return (
         <div className={appStyles.App}>
-            <ResetPasswordPage/>
+            <RouterProvider router={router}/>
         </div>
     );
 }
