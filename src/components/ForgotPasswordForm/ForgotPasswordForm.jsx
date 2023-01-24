c0cbaa5cf0975b2f2fe1a1440650e9a845807852
Darkerros <Darkerros@yandex.ru>
2023-01-25 02:15:26 +0300

    feat: add reset password functionality

--- src/components/ForgotPasswordForm/ForgotPasswordForm.jsx
+++ src/components/ForgotPasswordForm/ForgotPasswordForm.jsx
@@ -1,15 +1,23 @@
 import universalStyles from '../../styles/UniversalStyles.module.css'
 import React, {useState} from "react";
 import {Button, EmailInput} from "@ya.praktikum/react-developer-burger-ui-components";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import useUserController from "../../hooks/useUserController";
 
 const ForgotPasswordForm = () => {
     const [email, setEmail] = useState('')
-
+    const navigation = useNavigate()
+    const userController = useUserController()
     const onEmailChange = event => setEmail(event.target.value)
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        if (email) {
+            userController.resetPassword(email).then(() => navigation("/reset", {state: {from: "/forgot"}}))
+        }
+    }
 
     return (
-        <form className={universalStyles.form}>
+        <form className={universalStyles.form} onSubmit={onSubmit}>
             <h2 className={"text text_type_main-medium"}>Восстановление пароля</h2>
             <EmailInput value={email} onChange={onEmailChange} title={"Email"} extraClass={"mt-4"}/>
             <Button htmlType={"submit"} extraClass={"mt-4 mb-20"}>Восстановить</Button>
--- src/components/ResetPasswordForm/ResetPasswordForm.jsx
+++ src/components/ResetPasswordForm/ResetPasswordForm.jsx
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
 import universalStyles from "../../styles/UniversalStyles.module.css";
 import {Button, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import useUserController from "../../hooks/useUserController";
 
 const ResetPasswordForm = () => {
     const [password,setPassword] = useState('')
     const [code,setCode] = useState('')
+    const userController = useUserController()
+    const navigation = useNavigate()
 
     const onCodeChange = event => setCode(event.target.value)
     const onPasswordChange = event => setPassword(event.target.value)
 
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+        if (password && code) {
+            userController.resetPasswordAccept(password,code).then(() => navigation("/"))
+        }
+    }
+
 
     return (
-        <form className={universalStyles.form}>
+        <form className={universalStyles.form} onSubmit={onSubmit}>
             <h2 className={"text text_type_main-medium"}>Востановление пароля</h2>
             <PasswordInput value={password} onChange={onPasswordChange} placeholder={"Введите новый пароль"} extraClass={"mt-4"}/>
             <Input value={code} onChange={onCodeChange} placeholder={"Введите код из письма"} extraClass={"mt-4"}/>
