2530ea63f05f230e8300838790eb8ca66814a9b7
Darkerros <Darkerros@yandex.ru>
2023-01-25 01:50:21 +0300

    fix: test navlink

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,20 +1,21 @@
 import AppHeader from "../../components/AppHeader/AppHeader";
 import styles from './ProfilePage.module.css'
 import {EmailInput, Input, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
+import {NavLink} from "react-router-dom";
 const ProfilePage = () => {
     return (
         <>
             <AppHeader/>
             <div className={styles.container}>
-                <div>
-                    <div className={"text text_color_primary text_type_main-medium pt-4 pb-3"} >Профиль</div>
-                    <div className={"text text_color_inactive text_type_main-medium pt-4 pb-3"} >История заказов</div>
+                <div style={{display: "flex",flexDirection: "column"}}>
+                    <NavLink  to={"/profile"} className={({isActive}) => isActive ? "text text_color_primary text_type_main-medium pt-4 pb-3" : "text text_color_inactive text_type_main-medium pt-4 pb-3"} >Профиль</NavLink>
+                    <NavLink replace={true} to={"/profile/orders"} className={({isActive}) => isActive ? "text text_color_primary text_type_main-medium pt-4 pb-3 active" : "text text_color_inactive text_type_main-medium pt-4 pb-3"} >История заказов</NavLink>
                     <div className={"text text_color_inactive text_type_main-medium pt-4 pb-3"} >Выход</div>
                 </div>
                 <div>
-                    <Input value={""} onChange={""} name={"name"} type={"text"} placeholder={"Имя"} size={"small"}/>
-                    <EmailInput value={""} onChange={""} name={"email"} type={"email"} placeholder={"Почта"} extraClass={'mt-6'} size={"small"}/>
-                    <PasswordInput value={""} onChange={""} name={"password"} type={"password"} placeholder={"Пароль"} extraClass={'mt-6'} size={"small"}/>
+                    <Input value={""} onChange={""} name={"name"} type={"text"} placeholder={"Имя"} size={undefined} />
+                    <EmailInput value={""} onChange={""} name={"email"} type={"email"} placeholder={"Почта"} extraClass={'mt-6'} size={undefined}/>
+                    <PasswordInput value={""} onChange={""} name={"password"} type={"password"} placeholder={"Пароль"} extraClass={'mt-6'} size={undefined}/>
                 </div>
             </div>
         </>
