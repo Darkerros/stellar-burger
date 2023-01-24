25a2e25687596839f7dc0591236420f33082fa6f
Darkerros <Darkerros@yandex.ru>
2023-01-25 00:28:41 +0300

    feat: add private to route

--- src/pages/ResetPasswordPage/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage/ResetPasswordPage.jsx
@@ -1,14 +1,21 @@
 import universalStyles from '../../styles/UniversalStyles.module.css'
 import AppHeader from "../../components/AppHeader/AppHeader";
 import ResetPasswordForm from "../../components/ResetPasswordForm/ResetPasswordForm";
+import {Navigate, useLocation} from "react-router-dom";
 const ResetPasswordPage = () => {
+    const location = useLocation()
+
     return (
-        <>
-            <AppHeader/>
-            <div className={universalStyles.flexContainer}>
-                <ResetPasswordForm/>
-            </div>
-        </>
+        location.state?.from === "reset"
+            ?
+            <>
+                <AppHeader/>
+                <div className={universalStyles.flexContainer}>
+                    <ResetPasswordForm/>
+                </div>
+            </>
+            :
+            <Navigate to={"/"} />
     );
 };
 
