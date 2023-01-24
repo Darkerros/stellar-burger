360b8aedacdaa97bf2b7231d8cde827b5052d261
Darkerros <Darkerros@yandex.ru>
2023-01-25 00:58:22 +0300

    fix: rework check auth function and add get user function

--- src/hooks/useUserController.js
+++ src/hooks/useUserController.js
@@ -4,17 +4,23 @@ import Api from "../api/Api";
 const useUserController = () => {
   const tokenStorage = useTokenStorage()
   const updateRefreshToken = () => Api.updateToken(tokenStorage.getRefreshToken())
-  const checkAuth = () => Api.getUser(tokenStorage.getToken()).then(data => data.user).catch(() => updateRefreshToken().catch((err) => {
-    tokenStorage.setToken(null)
-    tokenStorage.setRefreshToken(null)
-    return Promise.reject(err)
-  }))
+      .then(data => {
+          const {user,accessToken,refreshToken} = data
+          tokenStorage.setRefreshToken(refreshToken)
+          tokenStorage.setToken(accessToken)
+          return user
+      })
+      .catch((err) => {
+          tokenStorage.setToken(null)
+          tokenStorage.setRefreshToken(null)
+          return Promise.reject(err)
+      })
 
-  const login = (email,password) => Api.login(email,password).then(data => {
-    tokenStorage.setToken(data.accessToken)
-    tokenStorage.setRefreshToken(data.refreshToken)
-    return data.user
-  })
+  const getUser = () => Api.getUser(tokenStorage.getToken()).then(data => data.user)
+
+  const checkAuth = () => getUser().catch(() => updateRefreshToken().then(() => getUser()))
+
+  const login = (email,password) => Api.login(email,password).then(data => data.user)
 
   const logout = () => Api.logout(tokenStorage.getRefreshToken()).then(() => {
     tokenStorage.setToken(null)
