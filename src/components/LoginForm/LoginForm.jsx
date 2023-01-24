7d2969589288669ae52728af987e5d95911d9371
Darkerros <Darkerros@yandex.ru>
2023-01-25 00:29:08 +0300

    feat: add login function

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,17 +1,34 @@
 import universalStyles from '../../styles/UniversalStyles.module.css'
 import {Button, EmailInput, PasswordInput} from "@ya.praktikum/react-developer-burger-ui-components";
 import {useState} from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import useUserController from "../../hooks/useUserController";
+import {useDispatch} from "react-redux";
+import {setUserAction} from "../../services/actions/userAction";
 const LoginForm = () => {
     const [password,setPassword] = useState('')
     const [email,setEmail] = useState('')
-
+    const userController = useUserController()
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const onEmailChange = event => setEmail(event.target.value)
     const onPasswordChange = event => setPassword(event.target.value)
 
+    const onSubmit = (evt) => {
+        evt.preventDefault()
+
+        if (email && password) {
+            userController.login(email,password)
+                .then(user => {
+                    dispatch(setUserAction(user))
+                    navigate(-1)
+                })
+        }
+    }
+
 
     return (
-        <form className={universalStyles.form}>
+        <form className={universalStyles.form} onSubmit={onSubmit}>
             <h2 className={"text text_type_main-medium"}>Вход</h2>
             <EmailInput value={email} onChange={onEmailChange} title={"Email"} extraClass={"mt-4"}/>
             <PasswordInput value={password} onChange={onPasswordChange} title={"Password"} extraClass={"mt-4"}/>
