6fda3f8c2c060a71123619e90f24845dd9bd7d17
Darkerros <Darkerros@yandex.ru>
2023-01-25 03:32:07 +0300

    fix: link style and route

--- src/components/ProfileMenu/ProfileMenu.js
+++ src/components/ProfileMenu/ProfileMenu.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import styles from './ProfileMenu.module.css'
 
 const ProfileMenu = () => {
+    const location = useLocation()
+
     return (
         <div style={{display: "flex",flexDirection: "column"}}>
-            <NavLink  to={"/profile"} className={({isActive}) => isActive ? "text text_color_primary text_type_main-medium pt-4 pb-3" : "text text_color_inactive text_type_main-medium pt-4 pb-3"} >Профиль</NavLink>
-            <NavLink replace={true} to={"/profile/orders"} className={({isActive}) => isActive ? "text text_color_primary text_type_main-medium pt-4 pb-3 active" : "text text_color_inactive text_type_main-medium pt-4 pb-3"} >История заказов</NavLink>
+            <Link replace={true} to={"/profile"} aria-activedescendant={"/profile "} className={location.pathname === '/profile' ? `text text_color_primary text_type_main-medium pt-4 pb-3 ${styles.link}` : `text text_color_inactive text_type_main-medium pt-4 pb-3 ${styles.link}`} >Профиль</Link>
+            <Link replace={true} to={"/profile/orders"} className={location.pathname === '/profile/orders' ? `text text_color_primary text_type_main-medium pt-4 pb-3 active ${styles.link}` : `text text_color_inactive text_type_main-medium pt-4 pb-3 ${styles.link}`} >История заказов</Link>
             <div className={"text text_color_inactive text_type_main-medium pt-4 pb-3"} >Выход</div>
         </div>
     );
