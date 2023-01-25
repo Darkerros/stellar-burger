b078e756eff01f0d9dfbb3c65cbf1708909f3d75
Darkerros <Darkerros@yandex.ru>
2023-01-25 03:23:13 +0300

    feat: add visible route

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -2,14 +2,22 @@ import AppHeader from "../../components/AppHeader/AppHeader";
 import styles from './ProfilePage.module.css'
 import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
 import UpdateProfileForm from "../../components/UpdateProfileForm/UpdateProfileForm";
+import {useLocation} from "react-router-dom";
 
 const ProfilePage = () => {
+    const location = useLocation()
+
     return (
         <>
             <AppHeader/>
             <div className={styles.container}>
                 <ProfileMenu/>
-                <UpdateProfileForm/>
+                {location.pathname === "/profile"
+                    ?
+                    <UpdateProfileForm/>
+                    :
+                    <div></div>
+                }
             </div>
         </>
     );
