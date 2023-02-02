1a108ab8fa1cfc6ae45a0056014740034b2fca3c
Darkerros <Darkerros@yandex.ru>
2023-02-03 01:07:31 +0300

    feat: add status to order card

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -3,7 +3,25 @@ import styles from './OrderCard.module.css'
 
 import priceIcon from '../../images/icons/money-icon.png'
 
-const OrderCard = ({id,date,title,ingredients,price,getIngredientImageFn}) => {
+const OrderCard = ({id,date,title,ingredients,price,getIngredientImageFn,status}) => {
+    const getStatus = (status) => {
+        switch (status) {
+
+            case "created":
+                return 'Создан'
+
+            case "pending":
+                return 'Готовится'
+
+            case "done":
+                return 'Выполнен'
+
+            default:
+                return "done"
+        }
+    }
+
+
     return (
         <div className={`pt-6 pb-6 pl-6 pr-6 ${styles.card}`}>
             <div className={styles.info}>
@@ -11,6 +29,7 @@ const OrderCard = ({id,date,title,ingredients,price,getIngredientImageFn}) => {
                 <p className={"text text_type_main-small text_color_inactive"}>{date}</p>
             </div>
             <p className={`text text_type_main-medium text_color_primary mt-6 ${styles.title}`}>{title}</p>
+            {status && <p className={status === "done" ? "text text_type_main-small mt-2 text_color_success" : status === "created" ? "text text_type_main-small mt-2 text_color_primary" :  "text text_type_main-small mt-2 text_color_accent"}>{getStatus(status)}</p>}
             <div className={`${styles.info} mt-6`}>
                 <div className={styles.ingredientsContainer}>
                     {ingredients.map((ingredientId,index) => ingredientId && <img key={`${id}-${index}-${ingredientId}`} src={getIngredientImageFn(ingredientId)} className={styles.ingredientImage} alt={"Картинка ингредиента"}/>)}
