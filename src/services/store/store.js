8d4c46a1eb3b1f1fb6b83c564b3b2e126cc73e0a
Darkerros <Darkerros@yandex.ru>
2023-02-02 18:56:22 +0300

    feat: add new order websocket midleware and order websocket reucer

--- src/services/store/store.js
+++ src/services/store/store.js
@@ -2,8 +2,9 @@ import {applyMiddleware, createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
+import {orderWebSocketMidleware} from "../midleware/orderWebSocketMidleware";
 
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk,orderWebSocketMidleware())))
 
 export default store;
