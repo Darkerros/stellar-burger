b58149a2ad066344d19a1bccf6a8c28fc7093e3a
Darkerros <Darkerros@yandex.ru>
2023-02-02 18:54:59 +0300

    feat: create order websocket midleware

---
+++ src/services/midleware/orderWebSocketMidleware.js
@@ -0,0 +1,39 @@
+
+import {
+    WEBSOCKET_CLOSE_CONNECTION,
+    WEBSOCKET_OPEN_CONNECTION, WEBSOCKET_SEND_MESSAGE,
+    webSocketConnectionClosedAction, webSocketConnectionErrorAction,
+    webSocketConnectionStartAction, webSocketGetMessageAction
+} from "../actions/webSocketActions";
+
+export const orderWebSocketMidleware = () => store => {
+    let socket = null;
+    return next => action => {
+        const {dispatch} = store
+        const {type,payload} = action
+
+        if (type === WEBSOCKET_OPEN_CONNECTION) {
+            socket = new WebSocket(payload)
+        }
+
+        if (socket) {
+
+            socket.onopen = event => dispatch(webSocketConnectionStartAction(event))
+            socket.onclose = event => dispatch(webSocketConnectionClosedAction(event))
+            socket.onerror = event => dispatch(webSocketConnectionErrorAction(event))
+            socket.onmessage = event => dispatch(webSocketGetMessageAction(JSON.parse(event.data)))
+
+            if (type === WEBSOCKET_SEND_MESSAGE) {
+                socket.send(JSON.stringify(payload))
+            }
+
+            if (type === WEBSOCKET_CLOSE_CONNECTION) {
+                console.log("START CLOSE")
+                socket.close(1000)
+                socket = null
+            }
+        }
+
+        next(action)
+    }
+}
