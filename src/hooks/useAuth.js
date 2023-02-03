ed5af9b9e28a93bbe23460a03677f62ded7398df
Darkerros <Darkerros@yandex.ru>
2023-02-04 02:00:19 +0300

    feat: add new user actions and states

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,7 +4,7 @@ import {useSelector} from "react-redux";
 const useAuth = () => {
     const user = useSelector(state => state.userReducer)
 
-    return {name: user.name,email: user.email, isAuth: !!user.email}
+    return {name: user.name,email: user.email, isAuth: !!user.email, isSuccess: user.isSuccess, isLoading: user.isLoading}
 }
 
 export default useAuth
