f66b21c236cce06572c32099aa99936bc89a00d6
Darkerros <Darkerros@yandex.ru>
2023-02-04 02:03:51 +0300

    fix: rework api check auth to check user thunk and add feed page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,24 +7,21 @@ import RegisterPage from "../../pages/RegisterPage/RegisterPage";
 import ForgotPasswordPage from "../../pages/ForgotPasswordPage/ForgotPasswordPage";
 import LoginPage from "../../pages/LoginPage/LoginPage";
 import {useDispatch} from "react-redux";
-import {resetUserAction, setUserAction} from "../../services/actions/userAction";
 import ProfilePage from "../../pages/ProfilePage/ProfilePage";
 import useUserController from "../../hooks/useUserController";
 import UnAuthRoute from "../UnAuthRoute/UnAuthRoute";
 import AuthRoute from "../AuthRoute/AuthRoute";
 import IngredientDetailsPage from "../../pages/IngredientDetailsPage/IngredientDetailsPage";
 import FeedPage from "../../pages/FeedPage/FeedPage";
+import {checkAuthThunk} from "../../services/actions/checkAuthThunk";
+import FeedDetailsPage from "../../pages/FeedDetailsPage/FeedDetailsPage";
 
 
 function App() {
     const dispatch = useDispatch()
     const userController = useUserController()
 
-    useEffect(() => {
-        userController.checkAuth()
-            .then(user => dispatch(setUserAction(user)))
-            .catch(() => dispatch(resetUserAction()))
-    },[dispatch, userController])
+    useEffect(() => dispatch(checkAuthThunk() as any),[dispatch, userController])
 
     const router = createBrowserRouter([
         {path: '/', element: <MainPage/>},
@@ -33,6 +30,7 @@ function App() {
         {path: "/reset-password", element: <UnAuthRoute><ResetPasswordPage/></UnAuthRoute>},
         {path: "/forgot-password", element: <UnAuthRoute><ForgotPasswordPage/></UnAuthRoute>},
         {path: "/feed", element: <FeedPage/>},
+        {path: "/feed/:id", element: <FeedDetailsPage/>},
         {path: "/profile/orders", element: <AuthRoute><ProfilePage/></AuthRoute>},
         {path: "/profile", element: <AuthRoute><ProfilePage/></AuthRoute>},
         {path: "/ingredients/:id", element: <IngredientDetailsPage/>},
