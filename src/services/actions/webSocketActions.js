abb0191ad45c9185db2d3a25b89a6013e6529e8f
Darkerros <Darkerros@yandex.ru>
2023-02-08 17:14:36 +0300

    fix: add event on close

--- src/services/actions/webSocketActions.js
+++ src/services/actions/webSocketActions.js
@@ -9,7 +9,7 @@ export const WEBSOCKET_SEND_MESSAGE = "WEBSOCKET_SEND_MESSAGE"
 export const webSocketConnectionStartAction = (event) => ({type: WEBSOCKET_CONNECTION_START, payload: event})
 export const webSocketOpenConnectionAction = (webSocketUrl) => ({type: WEBSOCKET_OPEN_CONNECTION, payload: webSocketUrl})
 export const webSocketConnectionClosedAction = (event) => ({type: WEBSOCKET_CONNECTION_CLOSED, payload: event})
-export const webSocketCloseConnectionAction = () => ({type: WEBSOCKET_CLOSE_CONNECTION})
+export const webSocketCloseConnectionAction = (event) => ({type: WEBSOCKET_CLOSE_CONNECTION,payload: event})
 export const webSocketConnectionErrorAction= (event) => ({type: WEBSOCKET_CONNECTION_ERROR, payload: event})
 export const webSocketGetMessageAction= (data) => ({type: WEBSOCKET_GET_MESSAGE, payload: data})
 export const webSocketSendMessageAction= (data) => ({type: WEBSOCKET_SEND_MESSAGE, payload: data})
