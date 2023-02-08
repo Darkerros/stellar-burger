8dd39c1223746ad5b75b24dedfdc2041ecaf1bd6
Darkerros <Darkerros@yandex.ru>
2023-02-08 17:17:19 +0300

    feat: add memoize to ingredient data hook and add new functionality

--- src/hooks/useIngredientsData.js
+++ src/hooks/useIngredientsData.js
@@ -1,8 +1,9 @@
 import {useSelector} from "react-redux";
 import {useCallback, useMemo} from "react";
+import {superIngredientsSelector} from "../services/selectors/ingredientsSelectors";
 
 export const useIngredientsData = () => {
-    const ingredients = useSelector(state => state.ingredientsReducer.ingredients)
+    const ingredients = useSelector(superIngredientsSelector)
 
     const ingredientsDataDict = useMemo(() => {
         const ingredientsData = {}
@@ -12,6 +13,8 @@ export const useIngredientsData = () => {
 
     const getIngredientImage = useCallback(ingredientId => ingredientsDataDict[ingredientId].image,[ingredientsDataDict])
     const getIngredientPrice = useCallback(ingredientId => ingredientsDataDict[ingredientId].price,[ingredientsDataDict])
+    const getIngredientData = useCallback(ingredientId => ingredientsDataDict[ingredientId],[ingredientsDataDict])
+    const calculateIngredientsPriceFromIdList = useCallback((ingredientsIdList) => ingredientsIdList.reduce((prev,ingredientId) => prev + getIngredientPrice(ingredientId),0),[getIngredientPrice])
 
-    return {ingredientsDataDict,getIngredientPrice,getIngredientImage}
+    return useMemo(() => ({ingredientsDataDict,getIngredientPrice,getIngredientImage,getIngredientData,calculateIngredientsPriceFromIdList}),[calculateIngredientsPriceFromIdList, getIngredientData, getIngredientImage, getIngredientPrice, ingredientsDataDict])
 }
