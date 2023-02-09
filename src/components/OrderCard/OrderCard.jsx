9950a9867c95e7ef98f3789d839122f989382144
Darkerros <Darkerros@yandex.ru>
2023-02-10 02:22:40 +0300

    fix: remove mass props and move getStatus to universal function and fix problem with show date

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -1,41 +1,31 @@
-import React from 'react';
+import React, {useCallback, useEffect, useMemo} from 'react';
 import styles from './OrderCard.module.css'
 
 import priceIcon from '../../images/icons/money-icon.png'
 import {Link} from "react-router-dom";
+import {useIngredientsData} from "../../hooks/useIngredientsData";
+import {getDate, getStatus} from "../../utils/utils";
 
-const OrderCard = ({id,number,date,title,ingredients,price,getIngredientImageFn,status,elementPosition}) => {
-    const getStatus = (status) => {
-        switch (status) {
+const OrderCard = ({elementPosition,orderInfo}) => {
+    const ingredientsData = useIngredientsData()
+    const price = useMemo(() => orderInfo.ingredients.reduce((a,ingredientId) => ingredientId ? a + ingredientsData.getIngredientPrice(ingredientId) : a,0),[ingredientsData, orderInfo.ingredients])
 
-            case "created":
-                return 'Создан'
+    const getLink = useCallback((linkPos) => linkPos === "feed" ? `/feed/${orderInfo._id}` : `/profile/orders/${orderInfo._id}`,[orderInfo])
 
-            case "pending":
-                return 'Готовится'
-
-            case "done":
-                return 'Выполнен'
-
-            default:
-                return "done"
-        }
-    }
-
-    const getLink = (linkPos) => linkPos === "feed" ? `/feed/${id}` : `/profile/orders/${id}`
+    useEffect(() => console.log("card"+orderInfo._id),[])
 
     return (
         <Link to={getLink(elementPosition)} className={styles.link}>
             <div className={`pt-6 pb-6 pl-6 pr-6 ${styles.card}`}>
                 <div className={styles.info}>
-                    <p className={"text text_type_main-small text_color_primary"}>{number}</p>
-                    <p className={"text text_type_main-small text_color_inactive"}>{date}</p>
+                    <p className={"text text_type_main-small text_color_primary"}>#{orderInfo.number}</p>
+                    <p className={"text text_type_main-small text_color_inactive"}>{getDate(orderInfo.createdAt)}</p>
                 </div>
-                <p className={`text text_type_main-medium text_color_primary mt-6 ${styles.title}`}>{title}</p>
-                {status && <p className={status === "done" ? "text text_type_main-small mt-2 text_color_success" : status === "created" ? "text text_type_main-small mt-2 text_color_primary" :  "text text_type_main-small mt-2 text_color_accent"}>{getStatus(status)}</p>}
+                <p className={`text text_type_main-medium text_color_primary mt-6 ${styles.title}`}>{orderInfo.name}</p>
+                {orderInfo.status && <p className={orderInfo.status === "done" ? "text text_type_main-small mt-2 text_color_success" : orderInfo.status === "created" ? "text text_type_main-small mt-2 text_color_primary" :  "text text_type_main-small mt-2 text_color_accent"}>{getStatus(orderInfo.status)}</p>}
                 <div className={`${styles.info} mt-6`}>
                     <div className={styles.ingredientsContainer}>
-                        {ingredients.map((ingredientId,index) => ingredientId && <img key={`${id}-${index}-${ingredientId}`} src={getIngredientImageFn(ingredientId)} className={styles.ingredientImage} alt={"Картинка ингредиента"}/>)}
+                        {orderInfo.ingredients.map((ingredientId,index) => ingredientId && <img key={`${orderInfo._id}-${index}-${ingredientId}`} src={ingredientsData.getIngredientImage(ingredientId)} className={styles.ingredientImage} alt={"Картинка ингредиента"}/>)}
                     </div>
                     <div className={styles.price}>
                         <p className={"text text_type_main-small text_color_primary"}>{price}</p>
