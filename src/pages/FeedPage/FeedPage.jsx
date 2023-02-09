978b61fcdceca786f6cc9d1f0e6671aa353cacf5
Darkerros <Darkerros@yandex.ru>
2023-02-10 02:26:16 +0300

    fix: move calculate price function and rework memoized orders state function

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -9,31 +9,24 @@ import {
     webSocketOpenConnectionAction
 } from "../../services/actions/webSocketActions";
 import {getIngredientsThunk} from "../../services/actions/getIngredientsThunk";
-import {useIngredientsData} from "../../hooks/useIngredientsData";
 import {superIngredientsSelector} from "../../services/selectors/ingredientsSelectors";
+import {websocketUrl} from "../../utils/websocketUrl";
+import {superOrderWebSocketReducerSelector} from "../../services/selectors/orderWebSocketSelectors";
 
 const FeedPage = () => {
     const dispatch = useDispatch()
     const ingredients = useSelector(superIngredientsSelector)
-    const {total,totalToday,orders} = useSelector(state => state.orderWebSocketReducer)
-    const {getIngredientImage,getIngredientPrice} = useIngredientsData()
+    const {total,totalToday,orders} = useSelector(superOrderWebSocketReducerSelector)
 
-    const {completeOrdersList,inWorkOrdersList} = useMemo(() => {
-        const ordersStates = {done: [], inWork : []}
-        orders.forEach(order => order.status === "done" ? ordersStates.done.push(order.number) : ordersStates.inWork.push(order.number))
-        return {completeOrdersList: ordersStates.done, inWorkOrdersList: ordersStates.inWork}
-    },[orders])
-
-    const calculatePrice = (ingredientsIdList) => ingredientsIdList.reduce((a,ingredientId) => ingredientId ? a + getIngredientPrice(ingredientId) : a,0)
+    const {completeOrdersList,inWorkOrdersList} = useMemo(() => orders.reduce((prev,order) => order.status === "done" ? {...prev, completeOrdersList: [...prev.completeOrdersList,order.number]} : {...prev, inWorkOrdersList: [...prev.inWorkOrdersList,order.number]},{completeOrdersList: [],inWorkOrdersList: []}),[orders])
 
     useEffect(() => {
         if (!ingredients.length) dispatch(getIngredientsThunk())
         // eslint-disable-next-line
     },[ingredients])
 
-
     useEffect(() => {
-        dispatch(webSocketOpenConnectionAction("wss://norma.nomoreparties.space/orders/all"))
+        dispatch(webSocketOpenConnectionAction(websocketUrl.allFeedUrl))
         return () => {dispatch(webSocketCloseConnectionAction())}
         // eslint-disable-next-line
     },[])
@@ -44,7 +37,7 @@ const FeedPage = () => {
             <h2 className={"text text_type_main-large text_color_primary mt-10 mb-5"}>Лента заказов</h2>
             <div className={styles.feedContainer}>
                 <div className={`${styles.feeds} pr-4`}>
-                            {orders.map(order => <OrderCard elementPosition={"feed"} number={`#${order.number}`} key={order.number} id={order._id} date={order.createAt} title={order.name} ingredients={order.ingredients} price={calculatePrice(order.ingredients)} getIngredientImageFn={getIngredientImage}/>)}
+                    {orders.map(order => <OrderCard elementPosition={"feed"} orderInfo={order} key={order._id}/>)}
                 </div>
                 <div className={"ml-15"}>
                     <OrdersWorkInfo completeOrdersId={completeOrdersList} inWorkOrdersId={inWorkOrdersList}/>
