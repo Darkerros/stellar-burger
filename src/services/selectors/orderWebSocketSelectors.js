1bc858a6be84c8a4cd2d3b939e4024f7503cbcbd
Darkerros <Darkerros@yandex.ru>
2023-02-10 20:06:12 +0300

    feat: remove all universal reducers files

--- src/services/selectors/orderWebSocketSelectors.js
+++
@@ -1,7 +0,0 @@
-import {createSelector} from "@reduxjs/toolkit";
-
-export const baseOrderWebSocketReducerSelector = (state) => state.orderWebSocketReducer
-export const baseOrderWebSocketOrdersSelector = (state) => state.orderWebSocketReducer.orders
-
-export const superOrderWebSocketReducerSelector = createSelector(baseOrderWebSocketReducerSelector,state => state)
-export const superOrderWebSocketOrdersSelector = createSelector(baseOrderWebSocketOrdersSelector,state => state)
