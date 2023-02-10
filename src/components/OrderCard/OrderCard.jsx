32a7f80c82818e26c89e89026d214aebf97f37ed
Darkerros <Darkerros@yandex.ru>
2023-02-10 18:08:21 +0300

    fix: remove console log

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -13,9 +13,9 @@ const OrderCard = ({elementPosition,orderInfo}) => {
     const price = useMemo(() => orderInfo.ingredients.reduce((a,ingredientId) => ingredientId ? a + ingredientsData.getIngredientPrice(ingredientId) : a,0),[ingredientsData, orderInfo])
 
     const getLink = useCallback((linkPos) => linkPos === "feed" ? `/feed/${orderInfo._id}` : `/profile/orders/${orderInfo._id}`,[orderInfo])
-    console.log(orderInfo)
+
     return (
-        <Link to={getLink(elementPosition)} className={styles.link}>
+        <Link to={getLink(elementPosition)} className={styles.link} >
             <div className={`pt-6 pb-6 pl-6 pr-6 ${styles.card}`}>
                 <div className={styles.info}>
                     <p className={"text text_type_main-small text_color_primary"}>#{orderInfo.number}</p>
