635ffd5960233e7acfb581023029b85dec684fca
Darkerros <Darkerros@yandex.ru>
2023-02-10 20:05:38 +0300

    feat: add new midleware

--- src/services/store/store.js
+++ src/services/store/store.js
@@ -2,9 +2,10 @@ import {applyMiddleware, createStore} from "redux";
 import {composeWithDevTools} from "redux-devtools-extension";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
-import {orderWebSocketMidleware} from "../midleware/orderWebSocketMidleware";
+import {userOrdersWebSocketMidleware} from "../midleware/userOrdersWebSocketMidleware";
+import {feedOrdersWebSocketMidleware} from "../midleware/feedOrdersWebSocketMidleware";
 
 
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk,orderWebSocketMidleware())))
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(thunk,userOrdersWebSocketMidleware(),feedOrdersWebSocketMidleware())))
 
 export default store;
