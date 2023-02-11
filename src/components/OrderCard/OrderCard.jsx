f88233ff63d97ef18a0cb9aba53d60d59723d88a
Darkerros <Darkerros@yandex.ru>
2023-02-11 18:00:22 +0300

    fix: styling component

--- src/components/OrderCard/OrderCard.jsx
+++ src/components/OrderCard/OrderCard.jsx
@@ -13,22 +13,26 @@ const OrderCard = ({elementPosition,orderInfo}) => {
     const price = useMemo(() => orderInfo.ingredients.reduce((a,ingredientId) => ingredientId ? a + ingredientsData.getIngredientPrice(ingredientId) : a,0),[ingredientsData, orderInfo])
 
     const getLink = useCallback((linkPos) => linkPos === "feed" ? `/feed/${orderInfo._id}` : `/profile/orders/${orderInfo._id}`,[orderInfo])
-
+    console.log(orderInfo)
     return (
         <Link to={getLink(elementPosition)} className={styles.link} state={{from: elementPosition,order: orderInfo}}>
+
             <div className={`pt-6 pb-6 pl-6 pr-6 ${styles.card}`}>
                 <div className={styles.info}>
-                    <p className={"text text_type_main-small text_color_primary"}>#{orderInfo.number}</p>
-                    <p className={"text text_type_main-small text_color_inactive"}>{getDate(orderInfo.createdAt)}</p>
+                    <p className={"text text_type_main-default text_color_primary"}>#{orderInfo.number}</p>
+                    <p className={"text text_type_main-default text_color_inactive"}>{getDate(orderInfo.createdAt)}</p>
                 </div>
                 <p className={`text text_type_main-medium text_color_primary mt-6 ${styles.title}`}>{orderInfo.name}</p>
                 {orderInfo.status && <p className={orderInfo.status === "done" ? "text text_type_main-small mt-2 text_color_success" : orderInfo.status === "created" ? "text text_type_main-small mt-2 text_color_primary" :  "text text_type_main-small mt-2 text_color_accent"}>{getStatus(orderInfo.status)}</p>}
                 <div className={`${styles.info} mt-6`}>
                     <div className={styles.ingredientsContainer}>
-                        {orderInfo.ingredients.map((ingredientId,index) => ingredientId && <img key={`${orderInfo._id}-${index}-${ingredientId}`} src={ingredientsData.getIngredientImage(ingredientId)} className={styles.ingredientImage} alt={"Картинка ингредиента"}/>)}
+                        {
+                            orderInfo.ingredients.slice(0,6)
+                                .map((ingredientId,index) => ingredientId && <div className={styles.ingredientImageContainer} data-count={`+${orderInfo.ingredients.slice(6).length}`} key={`${orderInfo._id}-${index}-${ingredientId}`}><img src={ingredientsData.getIngredientImage(ingredientId)} className={styles.ingredientImage} alt={"Картинка ингредиента"}/></div>)
+                        }
                     </div>
                     <div className={styles.price}>
-                        <p className={"text text_type_main-small text_color_primary"}>{price}</p>
+                        <p className={"text text_type_main-default text_color_primary"}>{price}</p>
                         <img src={priceIcon} alt="Иконка денег" className={styles.priceIcon}/>
                     </div>
                 </div>
